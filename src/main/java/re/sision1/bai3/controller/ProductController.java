0285eb53e0c0460123d2fafc92eea5de99acc356
haovu1663-crypto <haovu1663@gmail.com>
2026-03-26 14:05:14 +0700

    bô sing

--- src/main/java/re/sision1/bai3/controller/ProductController.java
+++ src/main/java/re/sision1/bai3/controller/ProductController.java
@@ -3,12 +3,13 @@ package re.sision1.bai3.controller;
 import lombok.Getter;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 import re.sision1.bai3.model.Product;
 import re.sision1.bai3.service.StudentService;
 
 import java.util.List;
 
-@Controller
+@RestController// trả về html hoăc json
 public class ProductController {
     private StudentService studentService;
 
